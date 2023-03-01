791cbef5b53fd903f2f7cda36842a32b43bb095b
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2023-03-01 23:35:46 +0200

    Initial Commit

---
+++ src/test/java/com/sphesp/contentcalendar/ContentCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sphesp.contentcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
